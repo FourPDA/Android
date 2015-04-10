752bafefacc93d14d9944e78b1bf309456d5333f
Anna Savinova <varannn@gmail.com>
2015-04-10 22:55:00 +0300

    Чуть поправлен вывод статей в списке.

---
+++ app/src/main/java/ru4pda/news/ui/ViewUtils.java
@@ -0,0 +1,19 @@
+package ru4pda.news.ui;
+
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by asavinova on 10/04/15.
+ */
+public class ViewUtils {
+
+	private static final String IMAGE_BASE_URL = "http://s.4pda.to/wp-content/uploads/headbands/213/phband-%s.jpg";
+
+	public static void loadImage(ImageView view, long id) {
+		Picasso.with(view.getContext())
+				.load(String.format(IMAGE_BASE_URL, id))
+				.into(view);
+	}
+}
--- app/src/main/java/ru4pda/news/ui/list/ArticlesAdapter.java
+++ app/src/main/java/ru4pda/news/ui/list/ArticlesAdapter.java
@@ -6,11 +6,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ru4pda.news.R;
 import ru4pda.news.dao.ArticleDao;
 import ru4pda.news.ui.CursorRecyclerViewAdapter;
+import ru4pda.news.ui.ViewUtils;
 
 /**
  * Created by asavinova on 10/04/15.
@@ -22,11 +24,13 @@ public class ArticlesAdapter extends CursorRecyclerViewAdapter<ArticlesAdapter.V
 	}
 
 	public static class ViewHolder extends RecyclerView.ViewHolder {
+		public ImageView imageView;
 		public TextView titleView;
 		public TextView descriptionView;
 
 		public ViewHolder(View view) {
 			super(view);
+			imageView = (ImageView) view.findViewById(R.id.image_view);
 			titleView = (TextView) view.findViewById(R.id.title_view);
 			descriptionView = (TextView) view.findViewById(R.id.description_view);
 		}
@@ -42,6 +46,8 @@ public class ArticlesAdapter extends CursorRecyclerViewAdapter<ArticlesAdapter.V
 
 	@Override
 	public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
+		long id = cursor.getLong(ArticleDao.Properties.Id.ordinal);
+		ViewUtils.loadImage(viewHolder.imageView, id);
 
 		String title = cursor.getString(ArticleDao.Properties.Title.ordinal);
 		viewHolder.titleView.setText(title);
