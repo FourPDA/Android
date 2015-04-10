f4c22873e57b4c53f2ac9b6c86074d0f325aedc1
Anna Savinova <varannn@gmail.com>
2015-04-11 01:09:54 +0300

    Внешние правки списка статей

--- app/src/main/java/ru4pda/news/ui/list/ArticlesAdapter.java
+++ app/src/main/java/ru4pda/news/ui/list/ArticlesAdapter.java
@@ -3,11 +3,15 @@ package ru4pda.news.ui.list;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ru4pda.news.R;
 import ru4pda.news.dao.ArticleDao;
@@ -19,6 +23,9 @@ import ru4pda.news.ui.ViewUtils;
  */
 public class ArticlesAdapter extends CursorRecyclerViewAdapter<ArticlesAdapter.ViewHolder> {
 
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+	private static final SimpleDateFormat VERBOSE_DATE_FORMAT = new SimpleDateFormat("dd MMMM yyyy");
+
 	public ArticlesAdapter(Context context, Cursor cursor) {
 		super(context, cursor);
 	}
@@ -26,12 +33,14 @@ public class ArticlesAdapter extends CursorRecyclerViewAdapter<ArticlesAdapter.V
 	public static class ViewHolder extends RecyclerView.ViewHolder {
 		public ImageView imageView;
 		public TextView titleView;
+		public TextView dateView;
 		public TextView descriptionView;
 
 		public ViewHolder(View view) {
 			super(view);
 			imageView = (ImageView) view.findViewById(R.id.image_view);
 			titleView = (TextView) view.findViewById(R.id.title_view);
+			dateView = (TextView) view.findViewById(R.id.date_view);
 			descriptionView = (TextView) view.findViewById(R.id.description_view);
 		}
 	}
@@ -52,7 +61,12 @@ public class ArticlesAdapter extends CursorRecyclerViewAdapter<ArticlesAdapter.V
 		String title = cursor.getString(ArticleDao.Properties.Title.ordinal);
 		viewHolder.titleView.setText(title);
 
+		String verboseDate = "";
+			Date date = new Date(cursor.getLong(ArticleDao.Properties.Date.ordinal));
+			verboseDate = VERBOSE_DATE_FORMAT.format(date);
+			viewHolder.dateView.setText(verboseDate);
+
 		String description = cursor.getString(ArticleDao.Properties.Description.ordinal);
-		viewHolder.descriptionView.setText(description);
+		viewHolder.descriptionView.setText(Html.fromHtml(description));
 	}
 }
