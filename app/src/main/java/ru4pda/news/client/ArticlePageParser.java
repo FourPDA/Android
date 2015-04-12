4784c5828f57c23a62d25b3e1b8a04a6ce3a645e
Pavel Savinov <swapii@gmail.com>
2015-04-12 23:16:08 +0300

    Упрощение парсинга страницы статьи.

--- app/src/main/java/ru4pda/news/client/ArticlePageParser.java
+++ app/src/main/java/ru4pda/news/client/ArticlePageParser.java
@@ -10,27 +10,19 @@ import ru4pda.news.client.model.FullArticle;
  */
 public class ArticlePageParser {
 
-	private static final Pattern CONTENT_PATTERN = Pattern.compile("<div class=\"content\">(.*?)<ul class=\"page-nav box\">", Pattern.DOTALL);
-	private static final Pattern CLEAR_MATERIALS_BOX_PATTERN = Pattern.compile("(.*?)<div class=\"materials-box\">", Pattern.DOTALL);
+	private static final Pattern CONTENT_PATTERN = Pattern.compile("width=\"1\" height=\"1\" /><p.*?style=\"text-align: justify.*?>(.*?)<br /></div>", Pattern.DOTALL);
 
 	public FullArticle parse(String pageSource) {
 		FullArticle article = new FullArticle();
 
 		Matcher matcher = CONTENT_PATTERN.matcher(pageSource);
 
-		if (matcher.find()) {
-			String dirtyContent = matcher.group(1);
-
-			Matcher materialsMatcher = CLEAR_MATERIALS_BOX_PATTERN.matcher(dirtyContent);
-			if (materialsMatcher.find()) {
-				article.setContent(materialsMatcher.group(1));
-			} else {
-				article.setContent(dirtyContent);
-			}
-
-		} else {
-			//TODO
+		if (!matcher.find()) {
+			throw new IllegalStateException("Can't parse page");
 		}
+
+		String content = matcher.group(1);
+		article.setContent(content);
 
 		return article;
 	}
