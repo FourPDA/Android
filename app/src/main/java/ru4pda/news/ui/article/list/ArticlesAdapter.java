6a70d3093788deb89212493281393665e4b2da4f
Pavel Savinov <swapii@gmail.com>
2015-04-12 15:40:01 +0300

    Картинка в списке новостей подстраивает свою высоту с сохранением пропорций.

--- app/src/main/java/ru4pda/news/ui/article/list/ArticlesAdapter.java
+++ app/src/main/java/ru4pda/news/ui/article/list/ArticlesAdapter.java
@@ -2,84 +2,34 @@ package ru4pda.news.ui.article.list;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.support.v7.widget.RecyclerView;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.Date;
 
 import ru4pda.news.R;
-import ru4pda.news.dao.ArticleDao;
 import ru4pda.news.ui.CursorRecyclerViewAdapter;
-import ru4pda.news.ui.ViewUtils;
-import ru4pda.news.ui.article.one.ArticleActivity_;
 
 /**
  * Created by asavinova on 10/04/15.
  */
-public class ArticlesAdapter extends CursorRecyclerViewAdapter<ArticlesAdapter.ViewHolder> {
+public class ArticlesAdapter extends CursorRecyclerViewAdapter<ViewHolder> {
+
+	private LayoutInflater inflater;
 
 	public ArticlesAdapter(Context context, Cursor cursor) {
 		super(context, cursor);
-	}
-
-	public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-		public ImageView imageView;
-		public TextView titleView;
-		public TextView dateView;
-		public TextView descriptionView;
-		private long id;
-		private String title;
-
-		public ViewHolder(View view) {
-			super(view);
-			view.setOnClickListener(this);
-
-			imageView = (ImageView) view.findViewById(R.id.image_view);
-			titleView = (TextView) view.findViewById(R.id.title_view);
-			dateView = (TextView) view.findViewById(R.id.date_view);
-			descriptionView = (TextView) view.findViewById(R.id.description_view);
-		}
-
-		@Override
-		public void onClick(View v) {
-			ArticleActivity_.intent(v.getContext())
-					.id(id)
-					.title(title)
-					.start();
-		}
-
-		public void setCursor(Cursor cursor) {
-			id = cursor.getLong(ArticleDao.Properties.Id.ordinal);
-			ViewUtils.loadImage(imageView, id);
-
-			title = cursor.getString(ArticleDao.Properties.Title.ordinal);
-			titleView.setText(title);
-
-			String verboseDate = "";
-			Date date = new Date(cursor.getLong(ArticleDao.Properties.Date.ordinal));
-			verboseDate = ViewUtils.VERBOSE_DATE_FORMAT.format(date);
-			dateView.setText(verboseDate);
-
-			String description = cursor.getString(ArticleDao.Properties.Description.ordinal);
-			descriptionView.setText(Html.fromHtml(description));
-		}
+		inflater = LayoutInflater.from(context);
 	}
 
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View itemView = LayoutInflater.from(parent.getContext())
-				.inflate(R.layout.article_list_item, parent, false);
-		ViewHolder viewHolder = new ViewHolder(itemView);
-		return viewHolder;
+		View view = inflater.inflate(R.layout.article_list_item, parent, false);
+		return new ViewHolder(view);
 	}
 
 	@Override
 	public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
 		viewHolder.setCursor(cursor);
 	}
+
 }
---
+++ app/src/main/java/ru4pda/news/ui/article/list/ViewHolder.java
@@ -0,0 +1,57 @@
+package ru4pda.news.ui.article.list;
+
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Date;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import ru4pda.news.R;
+import ru4pda.news.dao.ArticleDao;
+import ru4pda.news.ui.ViewUtils;
+import ru4pda.news.ui.article.one.ArticleActivity_;
+
+/**
+ * Created by pavel on 12/04/15.
+ */
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+	@InjectView(R.id.image_view) ImageView imageView;
+	@InjectView(R.id.title_view) TextView titleView;
+	@InjectView(R.id.date_view) TextView dateView;
+
+	public ViewHolder(View view) {
+		super(view);
+		ButterKnife.inject(this, view);
+	}
+
+	public void setCursor(Cursor cursor) {
+
+		final long id = cursor.getLong(ArticleDao.Properties.Id.ordinal);
+		final String title = cursor.getString(ArticleDao.Properties.Title.ordinal);
+		Date date = new Date(cursor.getLong(ArticleDao.Properties.Date.ordinal));
+
+		itemView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ArticleActivity_.intent(v.getContext())
+						.id(id)
+						.title(title)
+						.start();
+			}
+		});
+
+		String verboseDate = ViewUtils.VERBOSE_DATE_FORMAT.format(date);
+
+		ViewUtils.loadImage(imageView, id);
+		titleView.setText(title);
+		dateView.setText(verboseDate);
+
+	}
+
+}
