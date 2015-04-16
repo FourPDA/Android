6ba1410db5922f9f0fbce35922f728b030b7f27e
Anna Savinova <varannn@gmail.com>
2015-04-16 22:48:56 +0300

    Изменен экран О программе

--- app/src/main/java/ru4pda/news/ui/AboutActivity.java
+++ app/src/main/java/ru4pda/news/ui/AboutActivity.java
@@ -1,14 +1,9 @@
 package ru4pda.news.ui;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.MailTo;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.text.Html;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -23,7 +18,9 @@ import ru4pda.news.R;
 public class AboutActivity extends ActionBarActivity {
 
 	@ViewById Toolbar toolbar;
-	@ViewById WebView webView;
+	@ViewById TextView infoView;
+	@ViewById TextView userSwapi;
+	@ViewById TextView userVarann;
 
 	@AfterViews
 	void afterViews() {
@@ -31,61 +28,9 @@ public class AboutActivity extends ActionBarActivity {
 		setSupportActionBar(toolbar);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		WebViewClient webClient = new WebViewClient() {
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				if (url.startsWith("mailto:")) {
-					MailTo mailTo = MailTo.parse(url);
-					mailTo("mailto:" + mailTo.getTo(), mailTo.getSubject());
-					view.reload();
-					return true;
-				} else {
-					openLink(url);
-				}
-				return true;
-			}
-		};
-		webView.setWebViewClient(webClient);
-
-		webView.loadData(getHtml(), "text/html; charset=utf-8", null);
-	}
-
-	private void mailTo(String mailto, String subject) {
-		Intent intent = new Intent(Intent.ACTION_SENDTO);
-		intent.setType("text/plain");
-		intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-		intent.setData(Uri.parse(mailto));
-
-		Intent chooser = Intent.createChooser(intent, getString(R.string.chooser_title));
-		chooser.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		try {
-			startActivity(chooser);
-		} catch (ActivityNotFoundException ex) {
-			Toast.makeText(this, R.string.no_email_applications_installed, Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	private void openLink(String url) {
-		Intent intent = new Intent(Intent.ACTION_VIEW);
-		intent.setData(Uri.parse(url));
-
-		Intent chooser = Intent.createChooser(intent, getString(R.string.chooser_title));
-		chooser.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		try {
-			startActivity(chooser);
-		} catch (ActivityNotFoundException ex) {
-			Toast.makeText(this, R.string.no_browsers_installed, Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	private String getHtml() {
-		return "<div style=\"margin-left: auto; margin-right: auto;\">"
-				+"<p>Клиент для сайта <a href=\"http://4pda.ru\">http://4pda.ru</a></p>"
-				+ "<p>Разработчики:</p>"
-				+ "<ul>"
-				+ "<li>Павел Савинов &lt;<a href=\"mailto:swapii@gmail.com\">swapii@gmail.com</a>&gt;</li>"
-				+ "<li>Анна Савинова &lt;<a href=\"mailto:varannn@gmail.com\">varannn@gmail.com</a>&gt;</li>"
-				+ "</ul></div>";
+		infoView.setText(Html.fromHtml(getString(R.string.about_info)));
+		userSwapi.setText(Html.fromHtml(getString(R.string.about_user_swapi)));
+		userVarann.setText(Html.fromHtml(getString(R.string.about_user_varann)));
 	}
 
 }
