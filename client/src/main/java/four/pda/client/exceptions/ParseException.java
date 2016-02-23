04709946f8eafd90f4ec9dac2535e1b4eb050f85
Anna Savinova <varannn@gmail.com>
2016-02-23 15:04:40 +0300

    Строгие проверки в парсерах. Исправлен тест для обзоров.

---
+++ client/src/main/java/four/pda/client/exceptions/ParseException.java
@@ -0,0 +1,12 @@
+package four.pda.client.exceptions;
+
+/**
+ * Created by asavinova on 22/02/16.
+ */
+public class ParseException extends RuntimeException {
+
+	public ParseException(String message) {
+		super(message);
+	}
+
+}
