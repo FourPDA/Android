fac45054da7a3538375da578f40c74bb3f85b3b5
Anna Savinova <varannn@gmail.com>
2016-11-27 23:39:17 +0300

    Убираем Google Analytics

--- app/src/main/java/four/pda/analytics/ProxyTracker.java
+++ app/src/main/java/four/pda/analytics/ProxyTracker.java
@@ -1,40 +1,22 @@
 package four.pda.analytics;
 
-import android.app.Application;
 import android.content.Context;
-
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
 
 import java.util.Map;
 
-import four.pda.BuildConfig;
-
 /**
- * Трекер-прокси, переправляющий запросы в реальный трекер Google Analytics.
+ * Трекер-прокси, переправляющий запросы в реальный трекер аналитики.
  *
  * @author Pavel Savinov
  * @since 1.0.0
  */
 class ProxyTracker extends AnalyticsTracker {
 
-    private final Tracker tracker;
-
     public ProxyTracker(Context context) {
-
-		GoogleAnalytics analytics = GoogleAnalytics.getInstance(context);
-		analytics.enableAutoActivityReports((Application) context.getApplicationContext());
-		tracker = analytics.newTracker("UA-68992461-1");
-		tracker.enableAdvertisingIdCollection(true);
-		tracker.enableAutoActivityTracking(true);
-
-		String version = String.format("%s (%d)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
-		tracker.setAppVersion(version);
     }
 
     @Override
     public void send(Map<String, String> params) {
-        tracker.send(params);
     }
 
 }
